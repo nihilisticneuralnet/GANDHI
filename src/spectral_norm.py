ed235c4eda4572b122169363f2a3058dd33d23b2
卩ﾑ尺ｲん <138315505+nihilisticneuralnet@users.noreply.github.com>
2025-07-16 00:17:03 +0530

    Create spectral_norm.py

---
+++ src/spectral_norm.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SpectralNorm(nn.Module):
+    """Spectral Normalization wrapper"""
+    def __init__(self, module, name='weight', power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module = module
+        self.name = name
+        self.power_iterations = power_iterations
+        if not self._made_params():
+            self._make_params()
+
+    def _update_u_v(self):
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_bar")
+
+        height = w.data.shape[0]
+        for _ in range(self.power_iterations):
+            v.data = F.normalize(torch.mv(torch.t(w.view(height, -1).data), u.data), dim=0)
+            u.data = F.normalize(torch.mv(w.view(height, -1).data, v.data), dim=0)
+
+        sigma = u.dot(w.view(height, -1).mv(v))
+        setattr(self.module, self.name, w / sigma.expand_as(w))
+
+    def _made_params(self):
+        try:
+            u = getattr(self.module, self.name + "_u")
+            v = getattr(self.module, self.name + "_v")
+            w = getattr(self.module, self.name + "_bar")
+            return True
+        except AttributeError:
+            return False
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = nn.Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = nn.Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = F.normalize(u.data, dim=0)
+        v.data = F.normalize(v.data, dim=0)
+        w_bar = nn.Parameter(w.data)
+
+        del self.module._parameters[self.name]
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_bar", w_bar)
+
+    def forward(self, *args):
+        self._update_u_v()
+        return self.module.forward(*args)
+
+def spectral_norm(module, name='weight', power_iterations=1):
+    """Apply spectral normalization to a module"""
+    return SpectralNorm(module, name, power_iterations)
